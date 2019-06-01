1ba5d3e0dcd5f64dbaa1190b99b958af05d027dc
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-01 07:13:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,13 +57,13 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+
 # }
 
 	programmer_hash =
@@ -81,7 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]]
 
 end
 
